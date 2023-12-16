479f232938d48bc14ae8b697ab8bb6e33e69ecec
Dhruvin Bhalala <142414337+DhruvinBhalala@users.noreply.github.com>
2023-12-16 13:41:35 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Background-Color-Changer
+ In this project i create background color changer using js function and styling with Css
---
+++ app.js
@@ -0,0 +1,28 @@
+function red() {
+    document.getElementById("second").style.backgroundColor = "red";
+}
+
+function orange() {
+    document.getElementById("second").style.backgroundColor = "orange";
+}
+
+function yellow() {
+    document.getElementById("second").style.backgroundColor = "yellow";
+}
+
+function green() {
+    document.getElementById("second").style.backgroundColor = "green";
+}
+
+function blue() {
+    document.getElementById("second").style.backgroundColor = "blue";
+}
+
+function indigo() {
+    document.getElementById("second").style.backgroundColor = "indigo";
+}
+
+function violet() {
+    document.getElementById("second").style.backgroundColor = "violet";
+}
+
